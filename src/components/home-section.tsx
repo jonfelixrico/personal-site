9db40c2511ad77490f57cbf54417e72385c6ace9
Felix <jonfelixrico@gmail.com>
2023-04-08 00:40:32 +0800

    Add initial layout

--- src/components/home-section.tsx
+++ src/components/home-section.tsx
@@ -1,3 +1,20 @@
 export function HomeSection() {
-  return <div className="h-screen">home</div>
+  return (
+    <div className="h-screen flex flex-col justify-center">
+      <div className="mb-10 text-8xl font-sans font-semibold">
+        <div>Jon Felix Rico,</div>
+        <div>web developer.</div>
+      </div>
+
+      <div className="font-mono w-3/5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+    </div>
+  )
 }
