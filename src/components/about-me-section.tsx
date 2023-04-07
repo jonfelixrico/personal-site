495abac8b3c879d7b668ab47b906aa40306aaae9
Felix <jonfelixrico@gmail.com>
2023-04-08 01:19:09 +0800

    Adjust the content of the about me section

--- src/components/about-me-section.tsx
+++ src/components/about-me-section.tsx
@@ -1,45 +1,20 @@
 export function AboutMeSection() {
   return (
-    <div className="min-h-screen pt-20">
-      <div className="mb-5 flex flex-row items-center">
-        <div className="font-sans text-4xl font-semibold mr-5">Experience</div>
-        <div className="border-b flex-grow" />
-      </div>
+    <div className="min-h-screen grid grid-cols-1 grid-rows-6">
+      <div className="row-start-2 row-span-4">
+        <div className="mb-5 flex flex-row items-center">
+          <div className="font-sans text-4xl font-semibold mr-5">About Me</div>
+          <div className="border-b flex-grow" />
+        </div>
 
-      <div className="font-mono mb-5">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt.
-      </div>
-
-      <div className="font-mono mb-5">
-        Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-        suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
-        autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-        nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?
-      </div>
-
-      <div className="text-xl font-sans font-semibold mb-3">
-        Some of the tech I have worked with:
-      </div>
-
-      <div className="grid grid-cols-3 gap-3">
-        {/* todo add tech stack here */}
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
-        <div className="border">test</div>
+        <div className="font-mono mb-5">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt.
+        </div>
       </div>
     </div>
   )
