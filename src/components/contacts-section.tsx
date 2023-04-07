81d725d9b1532fe068d79ba1de2de31c6ba6813d
Felix <jonfelixrico@gmail.com>
2023-04-08 01:32:09 +0800

    Create the mock content for the other pages

--- src/components/contacts-section.tsx
+++ src/components/contacts-section.tsx
@@ -1,3 +1,21 @@
 export function ContactsSection() {
-  return <div className="min-h-screen">contacts</div>
+  return (
+    <div className="min-h-screen grid grid-cols-1 grid-rows-6">
+      <div className="row-start-2 row-span-4">
+        <div className="mb-5 flex flex-row items-center">
+          <div className="font-sans text-4xl font-semibold mr-5">Contacts</div>
+          <div className="border-b flex-grow" />
+        </div>
+
+        <div className="font-mono mb-5">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt.
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/showcase-section.tsx
+++ src/components/showcase-section.tsx
@@ -1,3 +1,25 @@
 export function ShowcaseSection() {
-  return <div className="min-h-screen">showcase</div>
+  return (
+    <div className="min-h-screen mt-14">
+      <div className="mb-5 flex flex-row items-center">
+        <div className="font-sans text-4xl font-semibold mr-5">Showcase</div>
+        <div className="border-b flex-grow" />
+      </div>
+
+      <div className="font-mono mb-5">
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+        et quas molestias excepturi sint occaecati cupiditate non provident,
+        similique sunt in culpa qui officia deserunt mollitia animi, id est
+        laborum et dolorum fuga.
+      </div>
+
+      <div className="gap-5 grid grid-cols-auto">
+        <div className="bg-gray-500 h-80"></div>
+        <div className="bg-gray-500 h-80"></div>
+        <div className="bg-gray-500 h-80"></div>
+        <div className="bg-gray-500 h-80"></div>
+      </div>
+    </div>
+  )
 }
