83cfa4db4c36472c6c61c00ad8824a79e892eef5
Felix <jonfelixrico@gmail.com>
2023-04-08 17:53:51 +0800

    Add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,29 @@ import HomeSectionAboutMe from '@/components/home/HomeSectionAboutMe'
 import HomeSectionContacts from '@/components/home/HomeSectionContacts'
 import HomeSectionExperience from '@/components/home/HomeSectionExperience'
 import HomeSectionMain from '@/components/home/HomeSectionMain'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <main className="snap-y snap-mandatory overflow-y-auto h-screen">
-      <section className="mx-auto max-w-screen-lg snap-always snap-start">
-        <HomeSectionMain />
-      </section>
-      <section className="mx-auto max-w-screen-lg snap-always snap-start">
-        <HomeSectionAboutMe />
-      </section>
-      <section className="mx-auto max-w-screen-lg snap-always snap-start">
-        <HomeSectionExperience />
-      </section>
-      <section className="mx-auto max-w-screen-lg snap-always snap-start">
-        <HomeSectionContacts />
-      </section>
-    </main>
+    <>
+      <Head>
+        <title>Jon Felix Rico</title>
+      </Head>
+
+      <main className="snap-y snap-mandatory overflow-y-auto h-screen">
+        <section className="mx-auto max-w-screen-lg snap-always snap-start">
+          <HomeSectionMain />
+        </section>
+        <section className="mx-auto max-w-screen-lg snap-always snap-start">
+          <HomeSectionAboutMe />
+        </section>
+        <section className="mx-auto max-w-screen-lg snap-always snap-start">
+          <HomeSectionExperience />
+        </section>
+        <section className="mx-auto max-w-screen-lg snap-always snap-start">
+          <HomeSectionContacts />
+        </section>
+      </main>
+    </>
   )
 }
