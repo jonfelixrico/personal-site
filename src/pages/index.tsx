9a65e5fd2bc9f99c79dff248f65719e7f2514811
Felix <jonfelixrico@gmail.com>
2023-04-08 17:20:23 +0800

    Use section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,18 @@ import HomeSectionMain from '@/components/home/HomeSectionMain'
 export default function Home() {
   return (
     <main className="snap-y snap-mandatory overflow-y-auto h-screen">
-      <div className="mx-auto max-w-screen-lg snap-always snap-start">
+      <section className="mx-auto max-w-screen-lg snap-always snap-start">
         <HomeSectionMain />
-      </div>
-      <div className="mx-auto max-w-screen-lg snap-always snap-start">
+      </section>
+      <section className="mx-auto max-w-screen-lg snap-always snap-start">
         <HomeSectionAboutMe />
-      </div>
-      <div className="mx-auto max-w-screen-lg snap-always snap-start">
+      </section>
+      <section className="mx-auto max-w-screen-lg snap-always snap-start">
         <HomeSectionExperience />
-      </div>
-      <div className="mx-auto max-w-screen-lg snap-always snap-start">
+      </section>
+      <section className="mx-auto max-w-screen-lg snap-always snap-start">
         <HomeSectionContacts />
-      </div>
+      </section>
     </main>
   )
 }
