098d30468713fc620bc432ae75b9e094aaac39b8
Felix <jonfelixrico@gmail.com>
2023-04-08 17:59:23 +0800

    Add some content to the portfolio page

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -7,7 +7,16 @@ export default function Portfolio() {
         <title>Jon Felix Rico • Portfolio</title>
       </Head>
 
-      <main>test</main>
+      <main>
+        <div className="mx-auto max-w-screen-lg mt-10">
+          <div className="mb-5 flex flex-row items-center">
+            <div className="font-sans text-4xl font-semibold mr-5">
+              Some of my work
+            </div>
+            <div className="border-b flex-grow" />
+          </div>
+        </div>
+      </main>
     </>
   )
 }
