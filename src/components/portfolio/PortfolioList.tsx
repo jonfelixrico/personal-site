20a1b7cb3609659cc39acf58eadac96aedb1c349
Felix <jonfelixrico@gmail.com>
2023-04-08 19:13:56 +0800

    Add placeholders

--- src/components/portfolio/PortfolioList.tsx
+++ src/components/portfolio/PortfolioList.tsx
@@ -1,10 +1,18 @@
+import PortfolioCard from './PortfolioCard'
+
 export default function PortfolioList() {
   return (
     <div className="grid grid-cols-1 grid-rows-auto gap-3">
-      <div className="bg-gray-500 h-40"></div>
-      <div className="bg-gray-500 h-10"></div>
-      <div className="bg-gray-500 h-40"></div>
-      <div className="bg-gray-500 h-40"></div>
+      <PortfolioCard
+        preview={<div className="bg-gray-500 h-40"></div>}
+        details={<div />}
+      />
+
+      <PortfolioCard
+        preview={<div className="bg-gray-500 h-40"></div>}
+        details={<div />}
+        reverse
+      />
     </div>
   )
 }
