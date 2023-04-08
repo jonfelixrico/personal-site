90b3e9e1813a9314bad21964687504b6fdef2ba0
Felix <jonfelixrico@gmail.com>
2023-04-08 16:15:37 +0800

    Apply changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,28 +7,30 @@ import ReactFullpage from '@fullpage/react-fullpage'
 export default function Home() {
   return (
     <main>
-      <ReactFullpage
-        credits={{}}
-        scrollingSpeed={250}
-        render={({ state, fullpageApi }) => {
-          return (
-            <ReactFullpage.Wrapper>
-              <div className="section">
-                <HomeSectionMain />
-              </div>
-              <div className="section">
-                <HomeSectionAboutMe />
-              </div>
-              <div className="section">
-                <HomeSectionExperience />
-              </div>
-              <div className="section">
-                <HomeSectionContacts />
-              </div>
-            </ReactFullpage.Wrapper>
-          )
-        }}
-      />
+      <div className="mx-auto max-w-screen-lg">
+        <ReactFullpage
+          credits={{}}
+          scrollingSpeed={250}
+          render={() => {
+            return (
+              <ReactFullpage.Wrapper>
+                <div className="section">
+                  <HomeSectionMain />
+                </div>
+                <div className="section">
+                  <HomeSectionAboutMe />
+                </div>
+                <div className="section">
+                  <HomeSectionExperience />
+                </div>
+                <div className="section">
+                  <HomeSectionContacts />
+                </div>
+              </ReactFullpage.Wrapper>
+            )
+          }}
+        />
+      </div>
     </main>
   )
 }
