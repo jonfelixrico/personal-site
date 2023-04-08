9c84bf2f7cadc488d3533efce84233d54ebae84a
Felix <jonfelixrico@gmail.com>
2023-04-08 18:41:23 +0800

    Apply fixes

--- src/components/portfolio/PortfolioCard.tsx
+++ src/components/portfolio/PortfolioCard.tsx
@@ -7,11 +7,11 @@ export default function PortfolioCard(props: {
   larger: 'left' | 'right'
 }) {
   return (
-    <div className="grid grid-col-5 rounded-lg drop-shadow-md">
-      <div className={props.larger === 'left' ? 'row-span-3' : 'row-span-2'}>
+    <div className="grid grid-cols-5 grid-rows-1 rounded-lg drop-shadow-md">
+      <div className={props.larger === 'left' ? 'col-span-3' : 'col-span-2'}>
         {props.left}
       </div>
-      <div className="row-auto">{props.right}</div>
+      <div className="col-auto">{props.right}</div>
     </div>
   )
 }
