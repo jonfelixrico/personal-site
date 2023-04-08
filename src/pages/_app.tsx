7e1c9aaf89c73e7f02711de10b67c0dcd9829016
Felix <jonfelixrico@gmail.com>
2023-04-09 01:03:16 +0800

    Apply to the entire app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header className="px-8 fixed w-screen">
+        <Navbar />
+      </header>
+      <Component {...pageProps} />
+    </>
+  )
 }
