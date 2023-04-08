9a8a866fb2628084bf4a3b6926e9b275112b2798
Felix <jonfelixrico@gmail.com>
2023-04-08 18:05:02 +0800

    Add content

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -7,13 +7,20 @@ export default function Portfolio() {
         <title>Jon Felix Rico • Portfolio</title>
       </Head>
 
-      <main>
-        <div className="mx-auto max-w-screen-lg mt-10">
+      <main className="h-screen pt-10">
+        <div className="mx-auto max-w-screen-lg">
           <div className="mb-5 flex flex-row items-center">
             <div className="font-sans text-4xl font-semibold mr-5">
               Some of my work
             </div>
             <div className="border-b flex-grow" />
+          </div>
+
+          <div className="grid grid-cols-2 grid-rows-auto gap-3">
+            <div className="bg-gray-500 h-40"></div>
+            <div className="bg-gray-500 h-10"></div>
+            <div className="bg-gray-500 h-40"></div>
+            <div className="bg-gray-500 h-40"></div>
           </div>
         </div>
       </main>
