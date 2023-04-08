adcc121aa72b7b6836a2706df52b1d9a655e49b4
Felix <jonfelixrico@gmail.com>
2023-04-08 16:09:00 +0800

    Integrate fullpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,33 @@ import HomeSectionAboutMe from '@/components/home/HomeSectionAboutMe'
 import HomeSectionContacts from '@/components/home/HomeSectionContacts'
 import HomeSectionExperience from '@/components/home/HomeSectionExperience'
 import HomeSectionMain from '@/components/home/HomeSectionMain'
+import ReactFullpage from '@fullpage/react-fullpage'
 
 export default function Home() {
   return (
     <main>
-      <div className="mx-auto max-w-screen-lg px-5">
-        <HomeSectionMain />
-        <HomeSectionAboutMe />
-        <HomeSectionExperience />
-        <HomeSectionContacts />
-      </div>
+      <ReactFullpage
+        credits={{}}
+        scrollingSpeed={250}
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <HomeSectionMain />
+              </div>
+              <div className="section">
+                <HomeSectionAboutMe />
+              </div>
+              <div className="section">
+                <HomeSectionExperience />
+              </div>
+              <div className="section">
+                <HomeSectionContacts />
+              </div>
+            </ReactFullpage.Wrapper>
+          )
+        }}
+      />
     </main>
   )
 }
