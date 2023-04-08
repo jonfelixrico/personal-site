8304ec46a42fe712d54b3fa7a6cb553749c42a22
Felix <jonfelixrico@gmail.com>
2023-04-08 16:16:54 +0800

    Remove the use of fullpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,34 +2,15 @@ import HomeSectionAboutMe from '@/components/home/HomeSectionAboutMe'
 import HomeSectionContacts from '@/components/home/HomeSectionContacts'
 import HomeSectionExperience from '@/components/home/HomeSectionExperience'
 import HomeSectionMain from '@/components/home/HomeSectionMain'
-import ReactFullpage from '@fullpage/react-fullpage'
 
 export default function Home() {
   return (
     <main>
       <div className="mx-auto max-w-screen-lg">
-        <ReactFullpage
-          credits={{}}
-          scrollingSpeed={250}
-          render={() => {
-            return (
-              <ReactFullpage.Wrapper>
-                <div className="section">
-                  <HomeSectionMain />
-                </div>
-                <div className="section">
-                  <HomeSectionAboutMe />
-                </div>
-                <div className="section">
-                  <HomeSectionExperience />
-                </div>
-                <div className="section">
-                  <HomeSectionContacts />
-                </div>
-              </ReactFullpage.Wrapper>
-            )
-          }}
-        />
+        <HomeSectionMain />
+        <HomeSectionAboutMe />
+        <HomeSectionExperience />
+        <HomeSectionContacts />
       </div>
     </main>
   )
