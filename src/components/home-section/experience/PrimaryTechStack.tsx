cf2a3961e80cd38f5d1f7812d3e0133cca4ff511
Felix <jonfelixrico@gmail.com>
2023-04-09 20:46:12 +0800

    Adjust how TechStackItem is used in the primary stack

--- src/components/home-section/experience/PrimaryTechStack.tsx
+++ src/components/home-section/experience/PrimaryTechStack.tsx
@@ -1,74 +1,88 @@
 import TechStackItem, { TechStackItemProps } from './TechStackItem'
 
-const ITEMS: TechStackItemProps[] = [
-  {
-    iconSrc: 'tech-stack/vue-js.svg',
-    name: 'Vue.js',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    items: [
-      {
-        iconSrc: 'tech-stack/quasar-framework.svg',
-        name: 'Quasar Framework',
-      },
-      {
-        iconSrc: 'tech-stack/typescript.svg',
-        name: 'TypeScript',
-      },
-    ],
-  },
-  {
-    iconSrc: 'tech-stack/nest-js.svg',
-    name: 'Nest.js',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    items: [
-      {
-        iconSrc: 'tech-stack/express-js.svg',
-        name: 'Express.js',
-      },
-      {
-        iconSrc: 'tech-stack/typescript.svg',
-        name: 'TypeScript',
-      },
-    ],
-  },
-  {
-    iconSrc: 'tech-stack/mongodb.svg',
-    name: 'MongoDB',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-  },
-  {
-    iconSrc: 'tech-stack/database.svg',
-    name: 'Relational Databases',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    items: [
-      {
-        iconSrc: 'tech-stack/postgresql.svg',
-        name: 'PostgreSQL',
-      },
-      {
-        iconSrc: 'tech-stack/mysql.svg',
-        name: 'MySQL',
-      },
-    ],
-  },
-]
+function ItemVue() {
+  const ITEMS = [
+    {
+      iconSrc: 'tech-stack/quasar-framework.svg',
+      name: 'Quasar Framework',
+    },
+    {
+      iconSrc: 'tech-stack/typescript.svg',
+      name: 'TypeScript',
+    },
+  ]
+
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/vue-js.svg"
+      name="Vue.js"
+      items={ITEMS}
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemNestJs() {
+  const ITEMS = [
+    {
+      iconSrc: 'tech-stack/express-js.svg',
+      name: 'Express.js',
+    },
+    {
+      iconSrc: 'tech-stack/typescript.svg',
+      name: 'TypeScript',
+    },
+  ]
+
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/nest-js.svg"
+      name="Nest.js"
+      items={ITEMS}
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemMongoDb() {
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/mongodb.svg"
+      name="MongoDB"
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemRelationalDatabases() {
+  const ITEMS = [
+    {
+      iconSrc: 'tech-stack/postgresql.svg',
+      name: 'PostgreSQL',
+    },
+    {
+      iconSrc: 'tech-stack/mysql.svg',
+      name: 'MySQL',
+    },
+  ]
+
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/database.svg"
+      name="Relational Databases"
+      items={ITEMS}
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
 
 export default function PrimaryTechStack() {
   return (
     <div className="flex flex-col gap-4">
-      {ITEMS.map((data) => (
-        <TechStackItem
-          iconSrc={data.iconSrc}
-          name={data.name}
-          items={data.items}
-          testimony={data.testimony}
-          key={data.name}
-        />
-      ))}
+      <ItemVue />
+      <ItemNestJs />
+      <ItemMongoDb />
+      <ItemRelationalDatabases />
     </div>
   )
 }
