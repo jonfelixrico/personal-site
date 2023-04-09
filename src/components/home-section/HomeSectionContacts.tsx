3c44b50a968f5c1281b5973058f66c945eaa5714
Felix <jonfelixrico@gmail.com>
2023-04-10 00:58:17 +0800

    Add lorem comment in the contact section

--- src/components/home-section/HomeSectionContacts.tsx
+++ src/components/home-section/HomeSectionContacts.tsx
@@ -5,14 +5,17 @@ export default function HomeSectionContacts(props: { className?: string }) {
   return (
     <div className={classnames('grid grid-rows-6', props.className)}>
       <div className="row-start-2 row-span-4">
-        <div className="mb-5 flex flex-row items-center">
+        <div className="mb-5">
           <FakeElement
             className="font-mono text-5xl font-semibold text-primary"
             name="contacts"
           />
+          <div className="font-mono text-secondary text-2xl">
+            &#47;&#47; lorem ipsum dolor
+          </div>
         </div>
 
-        <div className="mb-5">
+        <div className="mb-5 text-xl">
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
