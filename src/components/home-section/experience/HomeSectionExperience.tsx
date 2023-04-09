eff2606c1475303cfc754e899e9e1240589883d8
Felix <jonfelixrico@gmail.com>
2023-04-09 19:29:02 +0800

    Add discord.js

--- src/components/home-section/experience/HomeSectionExperience.tsx
+++ src/components/home-section/experience/HomeSectionExperience.tsx
@@ -95,6 +95,12 @@ const OTHERS: Tech[] = [
       },
     ],
   },
+  {
+    iconSrc: 'tech-stack/discord-js.svg',
+    name: 'Discord.js',
+    testimony:
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+  },
 ]
 
 export default function HomeSectionExperience() {
