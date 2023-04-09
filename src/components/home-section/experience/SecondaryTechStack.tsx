ef115d45c5bd5bdaa1a59b69b65da6dcefce98e1
Felix <jonfelixrico@gmail.com>
2023-04-09 20:56:00 +0800

    Make dedicated components per tech item

--- src/components/home-section/experience/SecondaryTechStack.tsx
+++ src/components/home-section/experience/SecondaryTechStack.tsx
@@ -1,95 +1,134 @@
-import { useState } from 'react'
+import { createElement, useState } from 'react'
 import TechStackItem, { TechStackItemProps } from './TechStackItem'
 
-const ITEMS: TechStackItemProps[] = [
-  {
-    iconSrc: 'tech-stack/angular.svg',
-    name: 'Angular',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    items: [
-      {
-        iconSrc: 'tech-stack/angular-material.svg',
-        name: 'Angular Material',
-      },
-      {
-        iconSrc: 'tech-stack/typescript.svg',
-        name: 'TypeScript',
-      },
-    ],
-  },
-  {
-    iconSrc: 'tech-stack/react.svg',
-    name: 'React',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    items: [
-      {
-        iconSrc: 'tech-stack/next-js.svg',
-        name: 'Next.js',
-      },
-      {
-        iconSrc: 'tech-stack/tailwind-css.svg',
-        name: 'TailwindCSS',
-      },
-      {
-        iconSrc: 'tech-stack/typescript.svg',
-        name: 'TypeScript',
-      },
-    ],
-  },
-  {
-    iconSrc: 'tech-stack/spring-boot.svg',
-    name: 'Spring Boot',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    items: [
-      {
-        iconSrc: 'tech-stack/java.svg',
-        name: 'Next.js',
-      },
-    ],
-  },
-  {
-    iconSrc: 'tech-stack/docker.svg',
-    name: 'Docker',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-  },
-  {
-    iconSrc: 'tech-stack/discord-js.svg',
-    name: 'Discord.js',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-  },
-  {
-    iconSrc: 'tech-stack/eventstoredb.svg',
-    name: 'EventStoreDB',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-  },
-  {
-    iconSrc: 'tech-stack/github-actions.svg',
-    name: 'Github Actions',
-    testimony:
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-  },
+function ItemAngular() {
+  const ITEMS = [
+    {
+      iconSrc: 'tech-stack/angular-material.svg',
+      name: 'Angular Material',
+    },
+    {
+      iconSrc: 'tech-stack/typescript.svg',
+      name: 'TypeScript',
+    },
+  ]
+
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/angular.svg"
+      name="Angular"
+      items={ITEMS}
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemReact() {
+  const ITEMS = [
+    {
+      iconSrc: 'tech-stack/next-js.svg',
+      name: 'Next.js',
+    },
+    {
+      iconSrc: 'tech-stack/tailwind-css.svg',
+      name: 'TailwindCSS',
+    },
+    {
+      iconSrc: 'tech-stack/typescript.svg',
+      name: 'TypeScript',
+    },
+  ]
+
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/react.svg"
+      name="React"
+      items={ITEMS}
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemSpringBoot() {
+  const ITEMS = [
+    {
+      iconSrc: 'tech-stack/java.svg',
+      name: 'Next.js',
+    },
+  ]
+
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/spring-boot.svg"
+      name="Spring Boot"
+      items={ITEMS}
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemDocker() {
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/docker.svg"
+      name="Docker"
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemDiscordJs() {
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/discord-js.svg"
+      name="Discord.js"
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemEventStoreDb() {
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/eventstoredb.svg"
+      name="EventStoreDB"
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+function ItemGithubActions() {
+  return (
+    <TechStackItem
+      iconSrc="tech-stack/github-actions.svg"
+      name="Github Actions"
+      testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    />
+  )
+}
+
+const TECH_ITEMS = [
+  ItemAngular,
+  ItemReact,
+  ItemSpringBoot,
+  ItemDocker,
+  ItemDiscordJs,
+  ItemEventStoreDb,
+  ItemGithubActions,
 ]
+
+function DynamicComponent(props: { is: () => JSX.Element }) {
+  return createElement(props.is)
+}
 
 const COLLAPSED_ITEM_COUNT = 4
 export default function SecondaryTechStack() {
   const [isExpanded, setExpanded] = useState(false)
   return (
     <div className="flex flex-col gap-4">
-      {ITEMS.slice(0, isExpanded ? undefined : COLLAPSED_ITEM_COUNT).map(
-        (data) => (
-          <TechStackItem
-            iconSrc={data.iconSrc}
-            name={data.name}
-            items={data.items}
-            testimony={data.testimony}
-            key={data.name}
-          />
+      {TECH_ITEMS.slice(0, isExpanded ? undefined : COLLAPSED_ITEM_COUNT).map(
+        (component, index) => (
+          <DynamicComponent is={component} key={index} />
         )
       )}
 
