61fd36226eec34bab5cfc4ca2c523d68ac9efbc8
Felix <jonfelixrico@gmail.com>
2023-04-10 00:40:38 +0800

    Adjust the intro section

--- src/components/home-section/HomeSectionMain.tsx
+++ src/components/home-section/HomeSectionMain.tsx
@@ -11,15 +11,21 @@ export default function HomeSectionMain(props: { className?: string }) {
         <FakeElement className="text-primary font-mono" name="web-developer" />
       </div>
 
-      <div className="w-4/6">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </div>
+      <FakeElement
+        className="w-5/6 text-xl"
+        name="intro"
+        tagClass="font-mono font-semibold text-accent"
+      >
+        <div className="ml-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </FakeElement>
     </div>
   )
 }
