795431dd9e1c9541c798bf77f7c2666b169d79b2
Felix <jonfelixrico@gmail.com>
2023-04-09 18:16:39 +0800

    Adjust TechStackItem

--- src/components/home-section/experience/TechStackItem.tsx
+++ src/components/home-section/experience/TechStackItem.tsx
@@ -8,13 +8,19 @@ export interface TechStackItemData {
 
 export default function TechStackItem(props: TechStackItemData) {
   return (
-    <div className="flex flex-row">
-      <div>
-        <Image alt={`Icon of ${props.name}`} src={props.iconSrc} />
+    <div>
+      <div className="flex flex-row gap-3 items-center mb-2">
+        <div className="relative h-10 w-10">
+          <Image alt={`Icon of ${props.name}`} src={props.iconSrc} fill />
+        </div>
+
+        <div className="text-xl font-semibold">{props.name}</div>
       </div>
-      <div className="flex-grow">
-        <div>{props.name}</div>
-        <div>{props.testimony}</div>
+      <div className="flex flex-row gap-3">
+        <div>
+          <div className="w-10" />
+        </div>
+        <div className="flex-grow">{props.testimony}</div>
       </div>
     </div>
   )
