67005565a0f730eaa361b2af25dd5ba4ef8f1cd6
Felix <jonfelixrico@gmail.com>
2023-04-10 01:45:20 +0800

    Invert the github actions icon

--- src/components/home-section/experience/SecondaryTechStack.tsx
+++ src/components/home-section/experience/SecondaryTechStack.tsx
@@ -103,6 +103,7 @@ function ItemGithubActions() {
     <TechStackItem
       iconSrc="tech-stack/github-actions.svg"
       name="Github Actions"
+      iconClass="invert"
       testimony="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
     />
   )
