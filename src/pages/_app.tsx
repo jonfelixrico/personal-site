8c675eec962c8f52505b59bd582e1bb60d3489e1
Felix <jonfelixrico@gmail.com>
2023-04-09 21:21:24 +0800

    Remove react-tooltip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,6 @@ import Navbar from '@/components/Navbar'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
-// https://react-tooltip.com/docs/getting-started#set-the-default-styling
-import 'react-tooltip/dist/react-tooltip.css'
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
