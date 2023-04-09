8cbe732079cfb1fefe151e9f76829ac03d5cf557
Felix <jonfelixrico@gmail.com>
2023-04-10 01:15:48 +0800

    Use fake element

--- src/components/home-section/experience/HomeSectionExperience.tsx
+++ src/components/home-section/experience/HomeSectionExperience.tsx
@@ -28,25 +28,31 @@ export default function HomeSectionExperience(props: { className?: string }) {
           </div>
         </div>
 
-        <div className="mb-10 text-xl">
-          <span>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt.
-          </span>
-          <br />
-          <br />
-          <span>
-            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
-            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-            nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-            voluptas nulla pariatur?
-          </span>
-        </div>
+        <FakeElement
+          name="summary"
+          className="mb-10 text-xl"
+          tagClass="font-mono text-accent"
+        >
+          <div className="ml-3">
+            <span>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </span>
+            <br />
+            <br />
+            <span>
+              Ut enim ad minima veniam, quis nostrum exercitationem ullam
+              corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+              consequatur? Quis autem vel eum iure reprehenderit qui in ea
+              voluptate velit esse quam nihil molestiae consequatur, vel illum
+              qui dolorem eum fugiat quo voluptas nulla pariatur?
+            </span>
+          </div>
+        </FakeElement>
 
         <div className="mb-5"></div>
 
