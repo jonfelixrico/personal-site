b26510de649e66e8e22fff589a79779ebe95e828
Felix <jonfelixrico@gmail.com>
2023-04-10 16:54:08 +0800

    Add the tech stack layout

--- src/components/portfolio/featured/FeaturedWisdom.tsx
+++ src/components/portfolio/featured/FeaturedWisdom.tsx
@@ -1,6 +1,11 @@
 import { useResizeDetector } from 'react-resize-detector'
 import { ConditionallyRender } from '@/components/ConditionallyRender'
 import Image from 'next/image'
+import IconWithTooltip from '@/components/IconWithTooltip'
+import cnFactory from 'classnames/bind'
+import styles from './TechStack.module.scss'
+
+const classnames = cnFactory.bind(styles)
 
 export default function FeaturedWisdom() {
   const { width, ref } = useResizeDetector({
@@ -20,18 +25,40 @@ export default function FeaturedWisdom() {
           vel sollicitudin vulputate, ultricies vel libero.
         </div>
 
-        <div className="flex flex-row gap-3">
-          <div className="h-8 w-8 relative">
-            <Image alt="test" src="tech-stack/vue-js.svg" fill />
-          </div>
-          <div className="h-8 w-8 relative">
-            <Image alt="test" src="tech-stack/react.svg" fill />
-          </div>
-          <div className="h-8 w-8 relative">
-            <Image alt="test" src="tech-stack/angular.svg" fill />
-          </div>
+        <div className={classnames('flex flex-row gap-3', styles['tech-list'])}>
+          <IconWithTooltip
+            src="tech-stack/quasar-framework.svg"
+            tooltipLabel="Quasar Framework"
+          />
+          <IconWithTooltip src="tech-stack/vue-js.svg" tooltipLabel="Vue.js" />
+          <IconWithTooltip
+            src="tech-stack/typescript.svg"
+            tooltipLabel="TypeScript"
+          />
+          <IconWithTooltip
+            src="tech-stack/nest-js.svg"
+            tooltipLabel="Nest.js"
+          />
+          <IconWithTooltip
+            src="tech-stack/discord-js.svg"
+            tooltipLabel="Discord.js"
+          />
+          <IconWithTooltip
+            src="tech-stack/spring-boot.svg"
+            tooltipLabel="Spring Boot"
+          />
+          <IconWithTooltip src="tech-stack/java.svg" tooltipLabel="Java" />
+          <IconWithTooltip
+            src="tech-stack/eventstoredb.svg"
+            tooltipLabel="EventStoreDB"
+          />
+          <IconWithTooltip
+            src="tech-stack/mongodb.svg"
+            tooltipLabel="MongoDB"
+          />
         </div>
       </div>
+
       <div className="col-span-8 flex flex-col justify-center" ref={ref}>
         <ConditionallyRender render={!!height}>
           <Image
