9d00d0c55b11f1394ddc5e2fb355513587c5faaf
Felix <jonfelixrico@gmail.com>
2023-04-10 17:30:28 +0800

    Remove the use of the stuff in HomeSectionMain

--- src/components/home-section/HomeSectionMain.tsx
+++ src/components/home-section/HomeSectionMain.tsx
@@ -11,21 +11,15 @@ export default function HomeSectionMain(props: { className?: string }) {
         <FakeElement className="text-primary font-mono" name="web-developer" />
       </div>
 
-      <FakeElement
-        className="w-5/6 text-xl"
-        name="intro"
-        tagClass="font-mono font-semibold text-accent"
-      >
-        <div className="ml-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </FakeElement>
+      <div className="w-5/6 text-xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
     </div>
   )
 }
