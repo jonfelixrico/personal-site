5a2d5a41177e2f9e7032aaae38221192cd77b066
Felix <jonfelixrico@gmail.com>
2023-04-10 16:38:44 +0800

    Tweak props

--- src/components/IconWithTooltip.tsx
+++ src/components/IconWithTooltip.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Tooltip from 'rc-tooltip'
+import classnames from 'classnames'
 
 export default function IconWithTooltip(props: {
   src: string
   alt?: string
   tooltipLabel: string
   className?: string
+  iconClass?: string
 }) {
   return (
     <Tooltip
@@ -14,12 +16,12 @@ export default function IconWithTooltip(props: {
       placement="top"
       mouseLeaveDelay={0}
     >
-      <div className="relative h-6 w-6 cursor-help">
+      <div className={classnames('relative cursor-pointer', props.className)}>
         <Image
           alt={props.alt ?? props.tooltipLabel}
           src={props.src}
           fill
-          className={props.className}
+          className={props.iconClass}
         />
       </div>
     </Tooltip>
