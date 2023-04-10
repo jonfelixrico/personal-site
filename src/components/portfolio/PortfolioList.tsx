86eb436d83d2f84ff3f7bbbe642659385a3b8565
Felix <jonfelixrico@gmail.com>
2023-04-11 00:27:54 +0800

    Create the section for marites

--- src/components/portfolio/PortfolioList.tsx
+++ src/components/portfolio/PortfolioList.tsx
@@ -1,10 +1,11 @@
+import FeaturedMarites from './featured/FeaturedMarites'
 import FeaturedWisdom from './featured/FeaturedWisdom'
 
 export default function PortfolioList() {
   return (
     <div className="flex flex-col gap-10">
       <FeaturedWisdom />
-      <FeaturedWisdom />
+      <FeaturedMarites />
     </div>
   )
 }
---
+++ src/components/portfolio/featured/FeaturedMarites.tsx
@@ -0,0 +1,40 @@
+import FeaturedTemplate from './FeaturedTemplate'
+
+export default function FeaturedMarites() {
+  return (
+    <FeaturedTemplate
+      projectName="Marites"
+      imageSrc="/portfolio/wisdom/panel/home.png"
+      details={
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          sollicitudin et tortor vel ullamcorper. Praesent dolor massa, dapibus
+          vel sollicitudin vulputate, ultricies vel libero.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          sollicitudin et tortor vel ullamcorper. Praesent dolor massa, dapibus
+          vel sollicitudin vulputate, ultricies vel libero.
+        </div>
+      }
+      techList={[
+        {
+          name: 'Quasar Framework',
+          iconSrc: 'tech-stack/quasar-framework.svg',
+        },
+        {
+          name: 'Vue.js',
+          iconSrc: 'tech-stack/vue-js.svg',
+        },
+        {
+          name: 'TypeScript',
+          iconSrc: 'tech-stack/typescript.svg',
+        },
+        {
+          name: 'PocketBase',
+          iconSrc: 'tech-stack/pocketbase.svg',
+        },
+      ]}
+    />
+  )
+}
