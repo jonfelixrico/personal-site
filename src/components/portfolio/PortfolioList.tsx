3366bb386580d07943916c4dce6e46c941435492
Felix <jonfelixrico@gmail.com>
2023-04-10 14:31:18 +0800

    Remove old portfolio list component

--- src/components/portfolio/PortfolioList.tsx
+++ src/components/portfolio/PortfolioList.tsx
@@ -1,75 +1,23 @@
 import { ReactNode } from 'react'
-import PortfolioCard from './PortfolioCard'
-
-function DetailsSection(props: {
-  header: string
-  techStack: string[]
-  descriptionSlot: ReactNode
-}) {
-  return (
-    <div className="p-3 flex flex-col h-full">
-      <div className="flex-grow">
-        <div className="font-sans text-xl">{props.header}</div>
-        <div className="mt-2 text-mono">{props.descriptionSlot}</div>
-      </div>
-
-      <div className="font-mono pt-5 text-xs">
-        {props.techStack.join(' • ')}
-      </div>
-    </div>
-  )
-}
+import { useResizeDetector } from 'react-resize-detector'
 
 export default function PortfolioList() {
+  const { width, ref } = useResizeDetector({
+    handleHeight: false,
+  })
+  const heightStyle = {
+    height: `${width}px`,
+  }
+
   return (
-    <div className="grid grid-cols-1 grid-rows-auto gap-3">
-      <PortfolioCard
-        preview={<div className="bg-gray-500 h-80"></div>}
-        details={DetailsSection({
-          header: 'Test Header',
-          techStack: ['tech1', 'tech2', 'tech3'],
-          descriptionSlot: (
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </div>
-          ),
-        })}
-      />
-
-      <PortfolioCard
-        preview={<div className="bg-gray-500 h-80"></div>}
-        details={DetailsSection({
-          header: 'Test Header',
-          techStack: ['tech1', 'tech2', 'tech3'],
-          descriptionSlot: (
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </div>
-          ),
-        })}
-      />
-
-      <PortfolioCard
-        preview={<div className="bg-gray-500 h-80"></div>}
-        details={DetailsSection({
-          header: 'Test Header',
-          techStack: ['tech1', 'tech2', 'tech3'],
-          descriptionSlot: (
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </div>
-          ),
-        })}
-      />
+    <div className="grid grid-cols-2 gap-3">
+      <div ref={ref} style={heightStyle}>
+        a
+      </div>
+      <div style={heightStyle}>b</div>
+      <div style={heightStyle}>c</div>
+      <div style={heightStyle}>d</div>
+      <div style={heightStyle}>e</div>
     </div>
   )
 }
