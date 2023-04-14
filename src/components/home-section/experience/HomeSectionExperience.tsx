2b348f68ad788771fae2a308be111ef979297237
Felix <jonfelixrico@gmail.com>
2023-04-15 01:24:08 +0800

    Move out the content for secondary list

--- src/components/home-section/experience/HomeSectionExperience.tsx
+++ src/components/home-section/experience/HomeSectionExperience.tsx
@@ -1,6 +1,7 @@
 import classnames from 'classnames'
 import Image from 'next/image'
 import { ReactNode } from 'react'
+import SecondaryTechContent from './SecondaryTechContent'
 
 function HeaderAndDescription(props: { className?: string }) {
   return (
@@ -188,86 +189,6 @@ function MainTechSection(props: { className?: string }) {
   )
 }
 
-const ICONS: Icon[] = [
-  {
-    src: 'icons/react.svg',
-    label: 'React',
-  },
-  {
-    src: 'icons/next-js.svg',
-    label: 'Next.js',
-  },
-  {
-    src: 'icons/tailwind-css.svg',
-    label: 'TailwindCSS',
-  },
-  {
-    src: 'icons/angular.svg',
-    label: 'Angular',
-  },
-  {
-    src: 'icons/express-js.svg',
-    label: 'Express.js',
-  },
-  {
-    src: 'icons/node-js.svg',
-    label: 'NodeJS',
-  },
-  {
-    src: 'icons/typescript.svg',
-    label: 'TypeScript',
-  },
-  {
-    src: 'icons/spring-boot.svg',
-    label: 'Spring Boot',
-  },
-  {
-    src: 'icons/eventstoredb.svg',
-    label: 'EventStoreDB',
-  },
-  {
-    src: 'icons/mysql.svg',
-    label: 'MySQL',
-  },
-  {
-    src: 'icons/java.svg',
-    label: 'Java',
-  },
-  {
-    src: 'icons/pocketbase.svg',
-    label: 'PocketBase',
-  },
-  {
-    src: 'icons/nginx.svg',
-    label: 'NGINX',
-  },
-  {
-    src: 'icons/discord-js.svg',
-    label: 'Discord.js',
-  },
-]
-
-function SecondaryTechSection(props: { className?: string }) {
-  return (
-    <div className={props.className}>
-      <h3 className="text-center text-4xl text-accent mb-5">
-        ...and others that I've fiddled with
-      </h3>
-
-      <div className="flex flex-row flex-wrap gap-6">
-        {ICONS.map(({ src, label }, index) => (
-          <div
-            key={index}
-            className="relative lg:h-24 lg:w-24 md:h-20 md:w-20 sm:h-16 sm:w-16 h-12 w-12"
-          >
-            <Image src={src} alt={label} fill />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
-
 export default function HomeSectionExperience(props: { className?: string }) {
   return (
     <div className={classnames('py-10 min-h-screen', props.className)}>
@@ -275,7 +196,14 @@ export default function HomeSectionExperience(props: { className?: string }) {
 
       <div className="bg-card py-8">
         <MainTechSection className="mx-auto max-w-screen-lg mb-20 px-5" />
-        <SecondaryTechSection className="mx-auto max-w-screen-lg px-5" />
+
+        <div className="mx-auto max-w-screen-lg px-5">
+          <h3 className="text-center text-4xl text-accent mb-5">
+            ...and others that I've fiddled with
+          </h3>
+
+          <SecondaryTechContent />
+        </div>
       </div>
     </div>
   )
