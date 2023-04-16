a5157358b7a048b9dce70a6a5331ed5e2b16e452
Felix <jonfelixrico@gmail.com>
2023-04-16 15:16:01 +0800

    Remove unused components

--- src/components/portfolio/featured/FeaturedTemplates.tsx
+++ src/components/portfolio/featured/FeaturedTemplates.tsx
@@ -37,14 +37,6 @@ interface Tech {
   name: string
 }
 
-export function FeaturedLinkIcon(props: { src: string; alt: string }) {
-  return (
-    <div className="w-10 h-10 relative">
-      <Image fill src={props.src} alt={props.alt} />
-    </div>
-  )
-}
-
 export function FeaturedLayout(props: {
   details: ReactNode
   title: string
