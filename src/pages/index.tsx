563622de74af994d209ec215fe4594b9e4adb0c0
Felix <jonfelixrico@gmail.com>
2023-04-16 17:43:52 +0800

    Integrate NavbarContainer into the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import HomeSectionMain from '@/components/home-section/HomeSectionMain'
 import Head from 'next/head'
 import HomeSectionPortfolio from '@/components/home-section/HomeSectionPortfolio'
 import ContactsOverlay from '@/components/contacts/ContactsOverlay'
-import Navbar from '@/components/layout/Navbar'
+import NavbarContainer, {
+  NavbarSectionIds,
+} from '@/components/layout/NavbarContainer'
 
 export default function Home() {
   return (
@@ -13,32 +15,26 @@ export default function Home() {
         <title>Jon Felix Rico • Home</title>
       </Head>
 
-      <div>
-        <header className="h-12 fixed w-screen px-8 z-10">
-          <Navbar className="h-full w-full" />
-        </header>
-        <main className="relative">
-          <ContactsOverlay classNames="absolute w-full h-full py-5 px-8" />
+      <NavbarContainer>
+        {/* TODO return this */}
+        {/* <ContactsOverlay classNames="absolute w-full h-full py-5 px-8" /> */}
 
-          <div className="h-screen overflow-auto">
-            <section id="section-main">
-              <HomeSectionMain />
-            </section>
+        <section id={NavbarSectionIds.HOME}>
+          <HomeSectionMain />
+        </section>
 
-            <section id="section-experience">
-              <HomeSectionExperience />
-            </section>
+        <section id={NavbarSectionIds.EXPERIENCE}>
+          <HomeSectionExperience />
+        </section>
 
-            <section id="section-portfolio">
-              <HomeSectionPortfolio />
-            </section>
+        <section id={NavbarSectionIds.PORTFOLIO}>
+          <HomeSectionPortfolio />
+        </section>
 
-            <section id="section-contacts">
-              <HomeSectionContacts className="mx-auto max-w-screen-lg min-h-screen section-px" />
-            </section>
-          </div>
-        </main>
-      </div>
+        <section id={NavbarSectionIds.CONTACTS}>
+          <HomeSectionContacts className="mx-auto max-w-screen-lg min-h-screen section-px" />
+        </section>
+      </NavbarContainer>
     </>
   )
 }
