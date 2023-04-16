5034bb87bb5b7d844dc399e951a7f33d5f619975
Felix <jonfelixrico@gmail.com>
2023-04-16 19:03:33 +0800

    Adjust HomeContent

--- src/components/section/HomeContent.tsx
+++ src/components/section/HomeContent.tsx
@@ -1,27 +1,74 @@
+import { ReactNode, useEffect, useRef } from 'react'
 import { NavbarSectionIds } from '../layout/Navbar'
 import HomeSectionContacts from './HomeSectionContacts'
 import HomeSectionExperience from './HomeSectionExperience'
 import HomeSectionMain from './HomeSectionMain'
 import HomeSectionPortfolio from './HomeSectionPortfolio'
+import { useIntersection } from 'react-use'
 
-export default function HomeContent(props: { className?: string }) {
+function SectionWrapper({
+  id,
+  onVisible,
+  children,
+}: {
+  id: string
+  children?: ReactNode
+  onVisible?: (id: string) => void
+}) {
+  const ref = useRef(null)
+  const intersection = useIntersection(ref, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  })
+
+  const ratio = intersection?.intersectionRatio ?? 0
+  useEffect(() => {
+    if (onVisible) {
+      onVisible(id)
+    }
+  }, [onVisible, ratio, id])
+
+  return (
+    <section ref={ref} id={id}>
+      {children}
+    </section>
+  )
+}
+
+export default function HomeContent(props: {
+  className?: string
+  onVisibleSectionChange?: (id: string) => void
+}) {
   return (
     <div className={props.className}>
-      <section id={NavbarSectionIds.HOME}>
+      <SectionWrapper
+        id={NavbarSectionIds.HOME}
+        onVisible={props.onVisibleSectionChange}
+      >
         <HomeSectionMain />
-      </section>
+      </SectionWrapper>
 
-      <section id={NavbarSectionIds.EXPERIENCE}>
+      <SectionWrapper
+        id={NavbarSectionIds.EXPERIENCE}
+        onVisible={props.onVisibleSectionChange}
+      >
         <HomeSectionExperience />
-      </section>
+      </SectionWrapper>
 
-      <section id={NavbarSectionIds.PORTFOLIO}>
+      <SectionWrapper
+        id={NavbarSectionIds.PORTFOLIO}
+        onVisible={props.onVisibleSectionChange}
+      >
         <HomeSectionPortfolio />
-      </section>
+      </SectionWrapper>
 
-      <section id={NavbarSectionIds.CONTACTS}>
-        <HomeSectionContacts className="mx-auto max-w-screen-lg min-h-screen section-px" />
-      </section>
+      <SectionWrapper
+        id={NavbarSectionIds.CONTACTS}
+        onVisible={props.onVisibleSectionChange}
+      >
+        <HomeSectionContacts className="mx-auto max-w-screen-lg min-h-screen Section-px" />
+      </SectionWrapper>
     </div>
   )
 }
