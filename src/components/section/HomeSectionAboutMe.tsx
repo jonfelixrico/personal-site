9fbc1e66a547100937d9560676890528f3d3b977
Felix <jonfelixrico@gmail.com>
2023-04-21 18:56:57 +0800

    Add content to the about me page

--- src/components/section/HomeSectionAboutMe.tsx
+++ src/components/section/HomeSectionAboutMe.tsx
@@ -8,12 +8,23 @@ export default function HomeSectionAboutMe() {
         </div>
 
         <div className="mb-5 text-xl">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt.
+          While I used to focus more on the back-end, my interests shifted when
+          I was introduced to Angular. Since then, my inclination for front-end
+          has only snowballed, although I still work on the back-end for my
+          personal projects.
+          <br />
+          <br />
+          Aside from building websites and applications, I also love to
+          integrate technology into my daily life. In fact, I once created an
+          automated garden and I'm constantly tinkering with my home network,
+          upgrading it with new tech whenever possible. If there's a way to
+          automate something, I'll definitely try it out.
+          <br />
+          <br />
+          In my free time, I used to be addicted to home brewing coffee, but I
+          recently quit because I was consuming at least a liter of coffee per
+          day. Now, I'm focused on exploring new ways to integrate tech into my
+          life and continuing to learn and grow as a developer.
         </div>
       </div>
     </div>
