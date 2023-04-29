d67a985f8af4a7910dd406afde2a897047ddca34
Felix <jonfelixrico@gmail.com>
2023-04-29 19:17:01 +0800

    Add the content

--- src/components/section/HomeSectionContacts.tsx
+++ src/components/section/HomeSectionContacts.tsx
@@ -1,20 +1,48 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+
+function ContactCard(props: { children?: ReactNode }) {
+  return <div className="bg-app-2 rounded-lg p-4">{props.children}</div>
+}
+
 export default function HomeSectionContacts() {
   return (
-    <div className="section-width section-px min-h-screen pt-10">
-      <div className="row-start-2 row-span-4 py-10">
-        <div className="mb-5">
-          <div className="text-5xl font-semibold text-primary">Contacts</div>
-          <div className="text-secondary text-2xl">Let&apos;s talk!</div>
-        </div>
+    <div
+      className="section-width section-px min-h-screen
+      pt-20 pb-10"
+    >
+      <div className="mb-5">
+        <div className="text-5xl font-semibold text-primary">Contacts</div>
+        <div className="text-secondary text-2xl">Let&apos;s get in touch!</div>
+      </div>
 
-        <div className="mb-5 text-xl">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt.
-        </div>
+      <div className="flex flex-col gap-4 items-stretch max-w-lg mx-auto">
+        <ContactCard>
+          <div className="flex flex-row gap-3 items-center">
+            <Image src="icons/linkedin.svg" alt="" height="50" width="50" />
+            <div className="flex-grow text-center">
+              <a className="text-2xl">linkedin.com/in/jonfelixrico</a>
+            </div>
+          </div>
+        </ContactCard>
+
+        <ContactCard>
+          <div className="flex flex-row gap-3 items-center">
+            <Image src="icons/github.svg" alt="" height="50" width="50" />
+            <div className="flex-grow text-center">
+              <a className="text-2xl">github.com/jonfelixrico</a>
+            </div>
+          </div>
+        </ContactCard>
+
+        <ContactCard>
+          <div className="flex flex-row gap-3 items-center">
+            <Image src="icons/email.svg" alt="" height="50" width="50" />
+            <div className="flex-grow text-center">
+              <a className="text-2xl">jonfelixrico@gmail.com</a>
+            </div>
+          </div>
+        </ContactCard>
       </div>
     </div>
   )
