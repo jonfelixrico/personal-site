a3ccaa1bf58728cbd2db2631c8bca5b292b057d1
Felix <jonfelixrico@gmail.com>
2023-04-29 18:04:28 +0800

    Create NavbarLayout

---
+++ src/components/layout/NavbarLayout.tsx
@@ -0,0 +1,22 @@
+import bindableCf from 'classnames/bind'
+import styles from './NavbarLayout.module.scss'
+import { ReactNode } from 'react'
+
+const classnames = bindableCf.bind(styles)
+
+export default function Navbar(props: {
+  className?: string
+  activeSection?: string
+  transparent?: boolean
+  children?: ReactNode
+}) {
+  return (
+    <nav
+      className={classnames(props.className, styles['nav-bar'], {
+        [styles['transparent']]: props.transparent,
+      })}
+    >
+      {props.children}
+    </nav>
+  )
+}
