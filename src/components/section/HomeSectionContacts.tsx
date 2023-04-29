cf443bebed7d0943ff6a8aee0cba745b1d1ce06d
Felix <jonfelixrico@gmail.com>
2023-04-29 18:51:55 +0800

    Add pt-10

--- src/components/section/HomeSectionContacts.tsx
+++ src/components/section/HomeSectionContacts.tsx
@@ -1,6 +1,6 @@
 export default function HomeSectionContacts() {
   return (
-    <div className="section-width section-px min-h-screen">
+    <div className="section-width section-px min-h-screen pt-10">
       <div className="row-start-2 row-span-4 py-10">
         <div className="mb-5">
           <div className="text-5xl font-semibold text-primary">Contacts</div>
