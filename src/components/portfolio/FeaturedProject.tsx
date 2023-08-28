a1aaf88cd1142056860e76aeadca53ae3f31313d
Felix <jonfelixrico@gmail.com>
2023-08-28 18:04:08 +0800

    Adjust link button

--- src/components/portfolio/FeaturedProject.tsx
+++ src/components/portfolio/FeaturedProject.tsx
@@ -38,7 +38,7 @@ function FitWidthImage(props: {
 }
 
 function LinkButton(props: {
-  icon?: ReactNode
+  icon: string
   label: string
   href: string
   className?: string
@@ -51,10 +51,12 @@ function LinkButton(props: {
         styles['link-button'],
         props.className,
         'rounded-md bg-app-1 overflow-hidden px-2 py-1',
-        'inline-flex flex-row items-center gap-2'
+        'inline-flex flex-row items-center gap-2',
+        'relative'
       )}
     >
-      {props.icon}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src={props.icon} alt={props.label} />
       <span className="font-medium text-sm">{props.label}</span>
     </a>
   )
