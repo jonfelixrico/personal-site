eb662f29e0892695b721ca4f2ccb380ccafd5e49
Felix <jonfelixrico@gmail.com>
2023-09-11 23:13:57 +0800

    Externalize LinkButton

--- src/components/portfolio/FeaturedProject.tsx
+++ src/components/portfolio/FeaturedProject.tsx
@@ -7,6 +7,7 @@ import styles from './FeaturedProject.module.scss'
 import bindableCf from 'classnames/bind'
 import Markdown from 'markdown-to-jsx'
 import { Project } from '@/types/project.interface'
+import { LinkButton } from '@/components/common/LinkButton'
 
 const classnames = bindableCf.bind(styles)
 
@@ -34,30 +35,6 @@ function FitWidthImage(props: {
         />
       </ConditionallyRender>
     </div>
-  )
-}
-
-function LinkButton(props: {
-  icon: string
-  label: string
-  href: string
-  className?: string
-}) {
-  return (
-    <a
-      href={props.href}
-      target="_blank"
-      className={classnames(
-        styles['link-button'],
-        props.className,
-        'rounded-md bg-app-1 overflow-hidden px-2 py-1',
-        'inline-flex flex-row items-center gap-2'
-      )}
-    >
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={props.icon} alt={props.label} />
-      <span className="font-medium text-sm">{props.label}</span>
-    </a>
   )
 }
 
