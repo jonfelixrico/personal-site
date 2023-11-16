9c0a8432e7f0ebc7e23548773034c2e351392360
Felix <jonfelixrico@gmail.com>
2023-11-17 03:39:28 +0800

    Add initial data for mockiyomi

--- src/models/projects/index.ts
+++ src/models/projects/index.ts
@@ -1,6 +1,7 @@
 import { Project } from '@/types/project.interface'
 import wisdom from './wisdom'
+import mockiyomi from './mockiyomi'
 
-const data: Project[] = [wisdom]
+const data: Project[] = [wisdom, mockiyomi]
 
 export default data
---
+++ src/models/projects/mockiyomi.ts
@@ -0,0 +1,39 @@
+import { Project } from '@/types/project.interface'
+import markdown from './wisdom.md'
+
+const project: Project = {
+  title: 'Mockiyomi',
+  description: markdown,
+  image: '/portfolio/wisdom/panel/home.png',
+
+  tech: [
+    {
+      label: 'Next.js',
+      icon: '/icons/next-js.svg',
+    }
+  ],
+
+  links: [
+    {
+      icon: 'icons/github.svg',
+      label: 'REST API',
+      url: 'https://github.com/jonfelixrico/wisdom-core-2022',
+    },
+    {
+      icon: 'icons/github.svg',
+      label: 'Discord Bot',
+      url: 'https://github.com/jonfelixrico/wisdom-bot-2022',
+    },
+    {
+      icon: 'icons/github.svg',
+      label: 'Web app front-end',
+      url: 'https://github.com/jonfelixrico/wisdom-panel-client-2022',
+    },
+    {
+      icon: 'icons/github.svg',
+      label: 'Web app back-end',
+      url: 'https://github.com/jonfelixrico/wisdom-panel-server-2022',
+    },
+  ],
+}
+export default project
