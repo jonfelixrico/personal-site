a23e3eb79d1df8a200e8d3afc67643c58884bf0a
Felix <jonfelixrico@gmail.com>
2024-03-28 03:44:45 +0800

    Replace CompatibiltiyPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
 import { Metadata } from 'next'
-import CompatibilityPage from './CompatibilityPage'
+import MainLayout from '@/modules/layout/MainLayout'
+import { SectionWrapper } from '@/modules/layout/SectionWrapper'
+import { Section } from '@/modules/layout/section.enum'
+import HomeSectionMain from '@/modules/section/HomeSectionMain'
+import ContactsOverlay from '@/modules/contacts/ContactsOverlay'
+import HomeSectionSkills from '@/modules/section/HomeSectionSkills'
+import HomeSectionPortfolio from '@/modules/section/HomeSectionPortfolio'
 
 export const metadata: Metadata = {
   title: 'Jon Felix Rico • Home',
 }
 
-
 export default function Home() {
-  return <CompatibilityPage />
+  return <MainLayout>
+    <SectionWrapper
+        id={Section.HOME}
+      >
+        <HomeSectionMain />
+      </SectionWrapper>
+
+      <ContactsOverlay>
+        <SectionWrapper
+          id={Section.SKILLS}
+        >
+          <HomeSectionSkills />
+        </SectionWrapper>
+
+        <SectionWrapper
+          id={Section.PORTFOLIO}
+        >
+          <HomeSectionPortfolio />
+        </SectionWrapper>
+      </ContactsOverlay>
+  </MainLayout>
 }
