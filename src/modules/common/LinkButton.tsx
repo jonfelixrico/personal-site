35eb617a190d8db4e5a5bce15a3f57e0268b88db
Felix <jonfelixrico@gmail.com>
2024-04-14 23:25:17 +0800

    fix: use next/Image instead of vanilla img tags (#69)
    * Use Next.js image instead of regular img
    
    * Remove unused eslint magic comments

--- src/modules/common/LinkButton.tsx
+++ src/modules/common/LinkButton.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
-
+import Image from 'next/image'
 import styles from './LinkButton.module.scss'
 import bindableCf from 'classnames/bind'
 
@@ -28,24 +26,15 @@ export function LinkButton({
         'inline-flex flex-row items-center gap-2',
       )}
     >
-      {/*
-        We're doing aria hidden here since we already have the label to provide the screen reader context
-
-        We're not using Next's Image component here since we expect that icons used here are SVGs. Next does
-        leaves them as is and does not apply any optimization.
-
-        The main reason why we're not using Next.js image component, though, is it does not accept em values for the width and height.
-      */}
-      <img
-        src={icon}
-        aria-hidden="true"
+      <div
         style={{
-          // We're going with 1em because we want the icon to have the same height as the text.
           width: '1em',
           height: '1em',
         }}
-        loading="lazy"
-      />
+        className="relative"
+      >
+        <Image src={icon} fill alt="" />
+      </div>
       <div>{label}</div>
     </a>
   )
