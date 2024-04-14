7df3e616bcf60fdf7d1df6e2053a3e23e0dc0f3d
Jon Felix Rico <jonfelixrico@gmail.com>
2024-04-14 20:40:51 +0800

    Introduce gap

--- src/modules/about/TechCarousel.tsx
+++ src/modules/about/TechCarousel.tsx
@@ -6,10 +6,12 @@ import { useMeasure } from 'react-use'
 
 export default function TechCarousel({
   icons,
-  iconSize,
+  iconSize = 100,
+  gap = 25,
 }: {
   icons: string[]
-  iconSize: number
+  iconSize?: number
+  gap?: number
 }) {
   const [ref, { width }] = useMeasure<HTMLDivElement>()
 
@@ -24,13 +26,14 @@ export default function TechCarousel({
 
   return (
     <div className="w-full select-none" ref={ref}>
-      <div className="overflow-clip flex flex-row gap-x-7" style={{ width }}>
-        {viewportIcons.map((iconSrc) => (
+      <div className="overflow-clip flex flex-row" style={{ width }}>
+        {viewportIcons.map((iconSrc, index) => (
           <div
             key={iconSrc}
             style={{
               width: iconSize,
               height: iconSize,
+              marginRight: index < viewportCount - 1 ? gap : 0,
             }}
             className="flex-none relative"
           >
