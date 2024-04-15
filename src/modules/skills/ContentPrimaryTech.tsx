bccf8703c2d2d5a5407f8bdf49b5af645d5299a5
Felix <jonfelixrico@gmail.com>
2024-04-16 01:18:47 +0800

    Externalize the component

--- src/modules/skills/ContentPrimaryTech.tsx
+++ src/modules/skills/ContentPrimaryTech.tsx
@@ -37,43 +37,7 @@ import {
   ICON_WEBPACK,
 } from '@/modules/common/icons'
 import TechChip from '@/modules/portfolio/TechChip'
-import classnames from 'classnames'
-import Image from 'next/image'
-import { ReactNode } from 'react'
-import { If, Then } from 'react-if'
-
-function ItemLayout(props: {
-  header: string
-  side?: ReactNode
-  className?: string
-  iconSrc: string
-  footer?: ReactNode
-  children: ReactNode
-}) {
-  return (
-    <div
-      className={classnames(
-        'p-5 rounded-lg bg-app-2 flex flex-col gap-3',
-        props.className,
-      )}
-    >
-      <div className="flex flex-row items-center gap-3 col-none">
-        <div className="relative h-12 w-12">
-          <Image alt={props.header} src={props.iconSrc} fill />
-        </div>
-        <h4 className="text-4xl text-white">{props.header}</h4>
-      </div>
-
-      <div className="flex-auto">{props.children}</div>
-
-      <If condition={!!props.footer}>
-        <Then>
-          <div className="flex-none flex flex-col gap-y-2">{props.footer}</div>
-        </Then>
-      </If>
-    </div>
-  )
-}
+import ItemLayout from '@/modules/skills/ItemLayout'
 
 function TechContainer({
   title,
