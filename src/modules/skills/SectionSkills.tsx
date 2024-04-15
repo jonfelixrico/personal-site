83176fb2dc3c2d213fb22faafc10f3f232a07a2e
Felix <jonfelixrico@gmail.com>
2024-04-16 01:36:28 +0800

    Move the content directly to SectionSkills

--- src/modules/skills/SectionSkills.tsx
+++ src/modules/skills/SectionSkills.tsx
@@ -1,4 +1,6 @@
-import ContentPrimaryTech from './ContentPrimaryTech'
+import SkillFrontEnd from '@/modules/skills/SkillFrontEnd'
+import SkillBackEnd from '@/modules/skills/SkillBackEnd'
+import SkillDevOps from '@/modules/skills/SkillDevOps'
 
 export default function SectionSkills() {
   return (
@@ -12,7 +14,13 @@ export default function SectionSkills() {
             </div>
           </div>
 
-          <ContentPrimaryTech />
+          <div className="grid lg:grid-cols-8 w-full gap-4">
+            <SkillFrontEnd className="lg:col-span-4" />
+
+            <SkillBackEnd className="lg:col-span-4" />
+
+            <SkillDevOps className="lg:col-span-4 lg:col-start-3" />
+          </div>
         </div>
       </div>
     </div>
