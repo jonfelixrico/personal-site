4c03eb40f4356bf596b95f1bd8ed6302c74a59be
Felix <jonfelixrico@gmail.com>
2024-04-21 16:16:28 +0800

    Create container

---
+++ src/modules/common/FadeInOnVisible.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import style from './FadeInOnVisible.module.scss'
+import cnBind from 'classnames/bind'
+import range from 'lodash/range'
+
+const classnames = cnBind.bind(style)
+
+import { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
+import { useIntersection } from 'react-use'
+
+export default function FadeInOnVisible({ children }: { children: ReactNode }) {
+  const ref = useRef<HTMLDivElement>(null)
+  const obs = useIntersection(ref, {
+    threshold: range(0, 11).map((val) => val * 0.1),
+  })
+
+  const [visible, setIsVisible] = useState(false)
+
+  const intersectionRatio = useMemo(() => obs?.intersectionRatio ?? 0, [obs])
+  useEffect(() => {
+    if (intersectionRatio > 0) {
+      setIsVisible(true)
+    }
+  }, [intersectionRatio, setIsVisible])
+
+  return (
+    <div
+      ref={ref}
+      className={classnames(style['container'], {
+        [style['not-visible']]: !visible,
+      })}
+    >
+      {children}
+    </div>
+  )
+}
