61430187cec6adae2caf270160f3b23453381589
Felix <jonfelixrico@gmail.com>
2024-04-21 17:00:45 +0800

    Use the OnVisibleOnce hook

--- src/modules/common/FadeInOnVisible.tsx
+++ src/modules/common/FadeInOnVisible.tsx
@@ -1,13 +1,12 @@
 'use client'
 
+import useVisibleOnce from '@/modules/common/visible-once.hook'
 import style from './FadeInOnVisible.module.scss'
 import cnBind from 'classnames/bind'
-import range from 'lodash/range'
 
 const classnames = cnBind.bind(style)
 
-import { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
-import { useIntersection } from 'react-use'
+import { ReactNode, useRef } from 'react'
 
 export default function FadeInOnVisible({
   children,
@@ -16,19 +15,9 @@ export default function FadeInOnVisible({
   children: ReactNode
   className?: string
 }) {
-  const [visible, setIsVisible] = useState(false)
-
   const ref = useRef<HTMLDivElement>(null)
-  const obs = useIntersection(ref, {
-    threshold: range(0, 11).map((val) => val * 0.1),
-  })
-  const intersectionRatio = useMemo(() => obs?.intersectionRatio ?? 0, [obs])
-  useEffect(() => {
-    if (intersectionRatio > 0) {
-      console.debug('Visible')
-      setIsVisible(true)
-    }
-  }, [intersectionRatio, setIsVisible])
+
+  const visible = useVisibleOnce(ref)
 
   return (
     <div
