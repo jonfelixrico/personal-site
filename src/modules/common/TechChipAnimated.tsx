a28dcae5d99f6ddce2db8715456aa7b2cdc3bb8c
Felix <jonfelixrico@gmail.com>
2024-04-21 17:19:21 +0800

    Created animated version of tech chip

---
+++ src/modules/common/TechChipAnimated.tsx
@@ -0,0 +1,29 @@
+import TechChip, { TechChipProps } from '@/modules/common/TechChip'
+import cnBind from 'classnames/bind'
+import style from './TechChipAnimated.module.scss'
+
+const classnames = cnBind.bind(style)
+
+export default function AnimatedTechChip({
+  isVisible,
+  delay = 0,
+  className,
+  ...techChipProps
+}: TechChipProps & {
+  isVisible?: boolean
+  delay?: number
+}) {
+  return (
+    <TechChip
+      {...techChipProps}
+      className={classnames(
+        className,
+        {
+          [style['not-visible']]: !isVisible,
+        },
+        style['animated-tech-chip'],
+        `delay-[${delay}ms]`,
+      )}
+    />
+  )
+}
