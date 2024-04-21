77caf1d7ec83f7e98423771f4bb93db83a3aeff5
Felix <jonfelixrico@gmail.com>
2024-04-21 17:47:51 +0800

    Add space

--- src/modules/layout/MainLayout.tsx
+++ src/modules/layout/MainLayout.tsx
@@ -56,7 +56,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
             All trademarks, logos and brand names are the property of their
             respective owners. All company, product and service names used in
             this website are for identification purposes only. Use of these
-            names,trademarks and brands does not imply endorsement.
+            names, trademarks and brands does not imply endorsement.
           </div>
         </div>
       </footer>
